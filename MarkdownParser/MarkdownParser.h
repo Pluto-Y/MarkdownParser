54eec65bcc5d1640ce710943cbddfdac2b92017d
Longhua Zhang <kuaileainits@163.com>
2017-09-16 11:14:23 +0800

    Initialized the project

---
+++ MarkdownParser/MarkdownParser.h
@@ -0,0 +1,19 @@
+//
+//  MarkdownParser.h
+//  MarkdownParser
+//
+//  Created by Pluto Y on 16/09/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarkdownParser.
+FOUNDATION_EXPORT double MarkdownParserVersionNumber;
+
+//! Project version string for MarkdownParser.
+FOUNDATION_EXPORT const unsigned char MarkdownParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkdownParser/PublicHeader.h>
+
+
